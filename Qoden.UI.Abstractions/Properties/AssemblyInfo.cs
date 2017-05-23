a59b46b79274dacc09a8fe2a9da707e8f0a75c78
Andrey Verbin <info@studiomobile.ru>
2017-05-23 18:11:50 +0200

    Use Qoden.UI.Abstractions as nuget packages instead of reference

--- Qoden.UI.Abstractions/Properties/AssemblyInfo.cs
+++ Qoden.UI.Abstractions/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Qoden.UI")]
+[assembly: AssemblyTitle("Qoden.UI.Abstractions")]
 [assembly: AssemblyDescription("Qoden UI utilities")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Qoden")]
