deded105c9c742c0914d7f18eab905d814b0b06d
Nicholas Yasnov <nickolay.yasnov@studiomobile.ru>
2017-09-01 22:54:01 +0300

    Add convenient methods for relative positioning in LayoutBox that receive reference LayoutBox

--- Shared/Layout/LayoutBox_Relative.cs
+++ Shared/Layout/LayoutBox_Relative.cs
@@ -14,6 +14,14 @@ namespace Qoden.UI
             box.Right = Pixel.Val(referenceOffset + dx.Value).Value;
             return box;
         }
+        
+        /// <summary>
+        /// Place this box before provided reference layout box. Reference is in view coordinate system.
+        /// </summary>
+        public static T Before<T>(this T box, ILayoutBox reference, Pixel dx) where T : ILayoutBox
+        {
+            return box.Before(reference.Frame(), dx);
+        }
 
         /// <summary>
         /// Place this box before provided reference box. Reference is in view coordinate system.
@@ -21,6 +29,14 @@ namespace Qoden.UI
         public static T Before<T>(this T box, RectangleF reference, float dx = 0) where T : ILayoutBox
         {
             return box.Before(reference, box.Unit.ToPixels(dx));
+        }
+
+        /// <summary>
+        /// Place this box before provided reference layout box. Reference is in view coordinate system.
+        /// </summary>
+        public static T Before<T>(this T box, ILayoutBox reference, float dx = 0) where T : ILayoutBox
+        {
+            return box.Before(reference.Frame(), dx);
         }
 
         /// <summary>
@@ -32,12 +48,29 @@ namespace Qoden.UI
             box.Left = Pixel.Val(referenceOffset + dx.Value).Value;
             return box;
         }
+
+        /// <summary>
+        /// Place this box after provided reference layout box. Reference is in view coordinate system.
+        /// </summary>
+        public static T After<T>(this T box, ILayoutBox reference, Pixel dx) where T : ILayoutBox
+        {
+            return box.After(reference.Frame(), dx);
+        }
+        
         /// <summary>
         /// Place this box after provided reference box. Reference is in view coordinate system.
         /// </summary>
         public static T After<T>(this T box, RectangleF reference, float dx = 0) where T : ILayoutBox
         {
             return box.After(reference, box.Unit.ToPixels(dx));
+        }
+        
+        /// <summary>
+        /// Place this box after provided reference box. Reference is in view coordinate system.
+        /// </summary>
+        public static T After<T>(this T box, ILayoutBox reference, float dx = 0) where T : ILayoutBox
+        {
+            return box.After(reference.Frame(), dx);
         }
 
         /// <summary>
@@ -51,12 +84,29 @@ namespace Qoden.UI
         }
 
         /// <summary>
+        /// Place this box below provided reference layout box. Reference is in view coordinate system.
+        /// </summary>
+        public static T Below<T>(this T box, ILayoutBox reference, Pixel dx) where T : ILayoutBox
+        {
+            return box.Below(reference.Frame(), dx);
+        }
+
+        /// <summary>
         /// Place this box below provided reference box. Reference is in view coordinate system.
         /// </summary>
         public static T Below<T>(this T box, RectangleF reference, float dx = 0) where T : ILayoutBox
         {
             return box.Below(reference, box.Unit.ToPixels(dx));
         }
+
+        /// <summary>
+        /// Place this box below provided reference layout box. Reference is in view coordinate system.
+        /// </summary>
+        public static T Below<T>(this T box, ILayoutBox reference, float dx = 0) where T : ILayoutBox
+        {
+            return box.Below(reference.Frame(), dx);
+        }
+        
         /// <summary>
         /// Place this box above provided reference box. Reference is in view coordinate system.
         /// </summary>
@@ -66,12 +116,29 @@ namespace Qoden.UI
             box.Bottom = Pixel.Val(referenceOffset + dx.Value).Value;
             return box;
         }
+        
+        /// <summary>
+        /// Place this box above provided reference layout box. Reference is in view coordinate system.
+        /// </summary>
+        public static T Above<T>(this T box, ILayoutBox reference, Pixel dx) where T : ILayoutBox
+        {
+            return box.Above(reference.Frame(), dx);
+        }
+        
         /// <summary>
         /// Place this box above provided reference box. Reference is in view coordinate system.
         /// </summary>
         public static T Above<T>(this T box, RectangleF reference, float dx = 0) where T : ILayoutBox
         {
             return box.Above(reference, box.Unit.ToPixels(dx));
+        }
+        
+        /// <summary>
+        /// Place this box above provided reference layout box. Reference is in view coordinate system.
+        /// </summary>
+        public static T Above<T>(this T box, ILayoutBox reference, float dx = 0) where T : ILayoutBox
+        {
+            return box.Above(reference.Frame(), dx);
         }
     }
 }
