6f74c8701640de03fca19214fa670e5871aaecb9
Max Evtukh <maksevtukh@gmail.com>
2017-10-31 23:13:41 +0300

    Added extension for getting visible view controller

---
+++ iOS/src/UIViewControllerExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UIKit;
+
+namespace Qoden.UI
+{
+    public static class UIViewControllerExtensions
+    {
+        public static UIViewController GetVisibleViewController(this UIViewController controller) 
+        { 
+            if (controller == null) 
+                controller = UIApplication.SharedApplication.KeyWindow.RootViewController; 
+ 
+            if (controller?.NavigationController?.VisibleViewController != null) 
+                return controller.NavigationController.VisibleViewController; 
+ 
+            if (controller.IsViewLoaded && controller.View?.Window != null) 
+                return controller; 
+            else 
+            { 
+                foreach (var childViewController in controller.ChildViewControllers) 
+                { 
+                    var foundVisibleViewController = GetVisibleViewController(childViewController); 
+                    if (foundVisibleViewController == null) 
+                        continue; 
+ 
+                    return foundVisibleViewController; 
+                } 
+            } 
+            return controller; 
+        } 
+    }
+}
