c1127afb3e318faa829057fb8a0e66debdd449a9
Nikita Shalin <dratemp16@gmail.com>
2018-04-27 03:07:15 +0300

    Add ActivityIndicator to Qoden.UI

---
+++ Droid/src/View/ActivityIndicator.cs
@@ -0,0 +1,53 @@
+﻿using Android.Content;
+using Android.Widget;
+using Android.Graphics.Drawables;
+
+
+namespace Qoden.UI
+{
+    public class ActivityIndicatorView : QodenView
+    {
+        ProgressBar ActivityIndicator { get; }
+
+        public ActivityIndicatorView(Context context) : base(context)
+        {
+            ActivityIndicator = Builder.Add<ProgressBar>();
+            ActivityIndicator.Indeterminate = true;
+        }
+
+        protected override void OnLayout(LayoutBuilder layout)
+        {
+            base.OnLayout(layout);
+            layout.Padding = new EdgeInsets(0, 20.Dp(), 0, 20.Dp());
+            var indicator = layout.View(ActivityIndicator)
+                                  .CenterHorizontally()
+                                  .CenterVertically()
+                                  .AutoSize();
+            layout.SetPreferredHeight(indicator.Height, true);
+        }
+
+        bool _dimBackground;
+
+        public bool DimBackground
+        {
+            get => _dimBackground;
+            set
+            {
+                if (_dimBackground != value)
+                {
+                    _dimBackground = value;
+                    Android.Graphics.Color bgColor;
+                    switch(Background)
+                    {
+                        case PaintDrawable paintDrawable: bgColor = paintDrawable.Paint.Color; break;
+                        case ColorDrawable colorDrawable: bgColor = colorDrawable.Color; break;
+                        default: bgColor = Android.Graphics.Color.Black; break;
+                    }
+                    bgColor.A = _dimBackground ? (byte)105 : (byte)255;
+                    var bg = new ColorDrawable(bgColor);
+                    Background = bg;
+                }
+            }
+        }
+    }
+}
---
+++ iOS/src/View/ActivityIndicator.cs
@@ -0,0 +1,43 @@
+﻿using UIKit;
+
+namespace Qoden.UI
+{
+    public class ActivityIndicatorView : QodenView
+    {
+        UIActivityIndicatorView ActivityIndicator { get; }
+
+
+        public ActivityIndicatorView()
+        {
+            ActivityIndicator = Builder.Add<UIActivityIndicatorView>();
+            ActivityIndicator.ActivityIndicatorViewStyle = UIActivityIndicatorViewStyle.WhiteLarge;
+            ActivityIndicator.StartAnimating();    
+        }
+
+        protected override void OnLayout(LayoutBuilder layout)
+        {
+            base.OnLayout(layout);
+            layout.Padding = new EdgeInsets(0, 20.Dp(), 0, 20.Dp());
+            var indicator = layout.View(ActivityIndicator)
+                                  .CenterHorizontally()
+                                  .CenterVertically()
+                                  .AutoSize();
+            layout.SetPreferredHeight(indicator.Height, true);
+        }
+
+        bool _dimBackground;
+
+        public bool DimBackground
+        {
+            get => _dimBackground;
+            set
+            {
+                if (_dimBackground != value)
+                {
+                    _dimBackground = value;
+                    BackgroundColor = BackgroundColor.ColorWithAlpha(_dimBackground ? 0.4f : 1);
+                }
+            }
+        }
+    }
+}
