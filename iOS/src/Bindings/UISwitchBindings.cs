4d70f429bb7ead9fa376c8459012ca1c34c74f03
tretikov <konstantin.tretikov@studiomobile.ru>
2018-05-25 16:51:54 +0300

    <develop> Fix ios switch checked property

--- iOS/src/Bindings/UISwitchBindings.cs
+++ iOS/src/Bindings/UISwitchBindings.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Foundation;
 using Qoden.Binding;
 using UIKit;
 
@@ -8,7 +9,21 @@ namespace Qoden.UI
     {
         public static IProperty<bool> CheckedProperty(this UISwitch label)
         {
-            return label.GetProperty(_ => _.On, KVCBindingStrategy.Instance);
+            return label.GetProperty(_ => _.On, SwitchCheckedBindingStrategy.Instance);
+        }
+    }
+    
+    public class SwitchCheckedBindingStrategy : IPropertyBindingStrategy
+    {
+        public static readonly SwitchCheckedBindingStrategy Instance = new SwitchCheckedBindingStrategy();
+        public IDisposable SubscribeToPropertyChange(IProperty property, Action<IProperty> action)
+        {
+            var tf = property.Owner as UISwitch;
+            tf.ValueChanged += (sender, eventArgs) => action(property);
+            return tf.AddObserver("on", NSKeyValueObservingOptions.OldNew, change =>
+            {
+                action(property);
+            });
         }
     }
 }
